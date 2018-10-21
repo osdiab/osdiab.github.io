44fec9a281be61f59087454ebb81fc653680514b
Omar Diab <me@omardiab.com>
2018-10-22 02:06:14 +0900

    Updates

---
+++ precache-manifest.67e2298d630820b9eaf6b3febc3888f2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "514c7c12632f29738ba1",
+    "url": "/static/js/main.514c7c12.chunk.js"
+  },
+  {
+    "revision": "61d3faa63a8aab5e7cfc",
+    "url": "/static/js/1.61d3faa6.chunk.js"
+  },
+  {
+    "revision": "be0e30a20629f518a4593611710e1d01",
+    "url": "/index.html"
+  }
+];
