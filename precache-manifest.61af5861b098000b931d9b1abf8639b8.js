18869ed3fcf994c518eedc965f2bb19abb95e82c
Omar Diab <od@eco.com>
2019-09-15 16:25:20 +0900

    Updates

---
+++ precache-manifest.61af5861b098000b931d9b1abf8639b8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f42c268d07aa7ee6945cb51bf940fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66cb2117ee97ca5d1749",
+    "url": "/static/js/2.f6682e25.chunk.js"
+  },
+  {
+    "revision": "255b17a25db777af7235",
+    "url": "/static/js/main.c000792f.chunk.js"
+  },
+  {
+    "revision": "586e6e5ad446f1b4e4b2",
+    "url": "/static/js/runtime~main.7579bab4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,14 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("/workbox-v3.6.2/workbox-sw.js");
-workbox.setConfig({modulePathPrefix: "/workbox-v3.6.2"});
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.16b05b8b612a0b411c6388ad6073bfce.js"
+  "/precache-manifest.61af5861b098000b931d9b1abf8639b8.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -26,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
