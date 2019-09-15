59b0885e849e019253c9e58766472787c21e6fb2
Omar Diab <od@eco.com>
2019-09-15 17:09:17 +0900

    edit rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
@@ -27,6 +28,15 @@ module.exports = {
     // from the extended configs.
     // If you add anything here, justify why.
 
-    "react/prop-types": 0 // not external facing library, so not needed for TypeScript
+    "react/prop-types": 0, // not external facing library, so not needed for TypeScript
+    "@typescript-eslint/explicit-function-return-type": 0, // inference makes code less wordy
+    "@typescript-eslint/no-empty-interface": 0, // useful for annotating function of types in progress
+    "@typescript-eslint/no-unused-vars": [
+      // Error on any unused variable, except for function arguments and for rest siblings
+      "error",
+      { vars: "all", args: "none", ignoreRestSiblings: true }
+    ],
+    "no-return-await": "error", // Useful for performance - makes sure there is no unnecessary unboxing
+    "no-restricted-imports": ["error", { patterns: ["../*", "./*"] }] // disallow relative imports
   }
 };
