29787ae56fbbd49b2fa3607896cc10e759402f0f
Omar Diab <od@eco.com>
2019-09-15 16:24:22 +0900

    reintroduce palette, with a highlighted primary color available

---
+++ src/@app/presentation/theme/palette.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  offWhite: "#faf9f0",
+  skyBlue: "#1dced8",
+  deepBlue: "#000249",
+  brightOrange: "#f6490d",
+  paleOrange: "#f97e53",
+  mediumGray: "#888888",
+  offBlack: "#2a2a2a"
+};
+
+export const palette = {
+  primary: colors.brightOrange,
+  primaryHighlight: colors.paleOrange,
+  background: colors.offWhite,
+  disabled: colors.mediumGray,
+  bodyText: colors.offBlack,
+  whiteText: colors.offWhite
+};
