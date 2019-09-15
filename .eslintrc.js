2da2703d41f4a7cd8a0154a3f943c035f7a93831
Omar Diab <od@eco.com>
2019-09-15 16:50:58 +0900

    more eslint defaults

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,10 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   extends: [
+    "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     "react-app",
     "plugin:react/recommended" // Uses the recommended rules from @eslint-plugin-react
   ],
