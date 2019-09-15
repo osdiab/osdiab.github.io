617e37f60f8471cdced96863cdb5ad5b8cc01786
Omar Diab <od@eco.com>
2019-09-15 16:51:22 +0900

    autofix some stuff

--- src/@app/utility/types.ts
+++ src/@app/utility/types.ts
@@ -1,4 +1,4 @@
 export type OptionalKeys<T> = {
-  [K in keyof T]-?: undefined extends T[K] ? K : never
+  [K in keyof T]-?: undefined extends T[K] ? K : never;
 }[keyof T];
 export type OptionalMembers<T> = { [K in OptionalKeys<T>]: T[K] };
