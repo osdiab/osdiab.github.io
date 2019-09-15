07b46fa193b728e38251504fd7cc1409a3a6e5be
Omar Diab <od@eco.com>
2019-09-15 16:48:04 +0900

    eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "react-app",
+    "plugin:react/recommended" // Uses the recommended rules from @eslint-plugin-react
+  ],
+  env: {
+    browser: true
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
+    }
+  },
+  settings: {
+    react: {
+      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+    }
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified
+    // from the extended configs.
+    // If you add anything here, justify why.
+
+    "react/prop-types": false // not external facing library, so not needed for TypeScript
+  }
+};
