aa2b969fd2b85f4170a90bf1b34b646206cfddac
Omar Diab <od@eco.com>
2019-09-16 14:42:22 +0900

    use macro version of styled-components

--- src/@app/presentation/theme/styled-components.ts
+++ src/@app/presentation/theme/styled-components.ts
@@ -1,6 +1,6 @@
 // this module alone is allowed to require styled-components directly
 // eslint-disable-next-line no-restricted-imports
-import * as styledComponents from "styled-components";
+import * as styledComponents from "styled-components/macro";
 
 import { ThemeInterface } from "./";
 
