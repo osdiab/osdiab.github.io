ddb48b9c10a92342662e6e057361bc9a9bb71130
Omar Diab <od@eco.com>
2019-09-16 15:30:01 +0900

    add route for component gallery

--- src/@app/Router/index.tsx
+++ src/@app/Router/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { BrowserRouter, Switch } from "react-router-dom";
+import loadable from "loadable-components";
 
 import { BlogPost } from "@app/presentation/pages/BlogPost";
 import { HomePage } from "@app/presentation/pages/HomePage";
@@ -11,6 +12,12 @@ export const Router: React.StatelessComponent<{}> = () => (
     <Switch>
       <DefaultLayoutRoute exact={true} path="/" component={HomePage} />
       <DefaultLayoutRoute path="/blog/:blogPostId" component={BlogPost} />
+      <DefaultLayoutRoute
+        path="/components"
+        component={loadable(() =>
+          import("@app/presentation/pages/ComponentGallery")
+        )}
+      />
       <DefaultLayoutRoute component={NotFound} />
     </Switch>
   </BrowserRouter>
---
+++ src/@app/presentation/pages/ComponentGallery/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ComponentGallery: React.FC = () => {
+  return <main></main>;
+};
+
+export default ComponentGallery;
