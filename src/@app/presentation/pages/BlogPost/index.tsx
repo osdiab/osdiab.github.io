3d1386c03a7b0af3b6c493295601288b1387ecb7
Omar Diab <me@omardiab.com>
2019-11-07 17:24:52 +0900

    utterances comments in blog posts

--- src/@app/presentation/pages/BlogPost/index.tsx
+++ src/@app/presentation/pages/BlogPost/index.tsx
@@ -9,8 +9,37 @@ export interface BlogPostRouteParams {
 }
 export type BlogPostProps = RouteComponentProps<BlogPostRouteParams>;
 
+export const UtterancesComments: React.FC = () => (
+  <script
+    src="https://utteranc.es/client.js"
+    repo="https://github.com/osdiab/osdiab.github.io"
+    issue-term="pathname"
+    label="blog-comment"
+    theme="github-light"
+    crossOrigin="anonymous"
+    async
+  ></script>
+);
+
+export const BlogPostLayout: React.FC = ({ children }) => {
+  return (
+    <>
+      <section>{children}</section>
+      <section>
+        <UtterancesComments />
+      </section>
+    </>
+  );
+};
+
 export const BlogPost: React.FC<BlogPostProps> = props => {
   const id = props.match.params.blogPostId;
   const Post = id in posts ? posts[id as keyof typeof posts] : undefined;
-  return Post ? <Post /> : <NotFound />;
+  return Post ? (
+    <BlogPostLayout>
+      <Post />
+    </BlogPostLayout>
+  ) : (
+    <NotFound />
+  );
 };
